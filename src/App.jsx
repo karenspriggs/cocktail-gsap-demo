ffe245e4db5a408824c761e66d2911e2aa46c2c6
Karen Spriggs <k.spriggs01@gmail.com>
2025-07-05 14:17:47 -0500

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <Navbar />
+        </main>
     )
 }
 export default App
