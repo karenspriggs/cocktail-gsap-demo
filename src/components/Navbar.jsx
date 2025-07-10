6c9dc6781617e5bce610a53b8f3ef3d43072b9eb
Karen Spriggs <61596604+karenspriggs@users.noreply.github.com>
2025-07-10 11:54:33 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         });
 
         navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
-            backgroundColor: '#00000050',
+            backgroundColor: '#00000080',
             backgroundFilter: 'blur(10px)',
             duration: 1,
             ease: 'power1.inOut'
